14fa13f35d735f01b0c473e5bfd90f21493fb32b
Vishal <vishal.hall@kcl.ac.uk>
2022-09-26 20:03:22 +0100

    Complete primes method

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    count = 2
+    while True:
+        if len(list) == number_of_primes:
+            break
+        
+        isPrime = True # Assume number is prime
+        for i in range(2, count):
+            if count % i == 0:
+                isPrime = False
+        
+        if isPrime:
+            list += [count]
+        count += 1
+
     return list
+
+print(primes(20))
